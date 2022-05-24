e3892a7f816cac697ddbcbaea652e5c7e10dde12
orangain <orangain@gmail.com>
2022-05-24 12:41:35 +0900

    Refine formula

--- Formula/ktcodeshift.rb
+++ Formula/ktcodeshift.rb
@@ -1,14 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ktcodeshift < Formula
-  desc "ktcodeshift is an experimental Kotlin port of jscodeshift."
-  homepage ""
+  desc "Toolkit for running codemods over multiple Kotlin files"
+  homepage "https://github.com/orangain/ktcodeshift"
   url "https://github.com/orangain/ktcodeshift/releases/download/0.1.0/ktcodeshift-0.1.0.tar.gz"
   sha256 "e3cd77af3842a7746b6b5957abe41b51b6422c603aa17cf905e68151e46fbb19"
   license "MIT"
 
-  # depends_on "cmake" => :build
+  depends_on "openjdk" => [:build, :optional]
 
   def install
     rm_f Dir["bin/*.bat"]
