aa0c603da4c460922d8bf21f52c894eba5d364c0
orangain <orangain@gmail.com>
2022-05-24 12:44:01 +0900

    Update description

--- Formula/ktcodeshift.rb
+++ Formula/ktcodeshift.rb
@@ -1,5 +1,5 @@
 class Ktcodeshift < Formula
-  desc "Toolkit for running codemods over multiple Kotlin files"
+  desc "Toolkit for running codemods over multiple Kotlin files inspired by jscodeshift"
   homepage "https://github.com/orangain/ktcodeshift"
   url "https://github.com/orangain/ktcodeshift/releases/download/0.1.0/ktcodeshift-0.1.0.tar.gz"
   sha256 "e3cd77af3842a7746b6b5957abe41b51b6422c603aa17cf905e68151e46fbb19"
