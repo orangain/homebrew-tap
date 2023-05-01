96c0a207eea83354b40cd3229ef19655ae223c6e
orangain <orangain@gmail.com>
2023-05-01 20:14:25 +0900

    ktcodeshift 0.2.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ktcodeshift.rb
+++ Formula/ktcodeshift.rb
@@ -1,8 +1,8 @@
 class Ktcodeshift < Formula
   desc "Toolkit for running codemods over multiple Kotlin files inspired by jscodeshift"
   homepage "https://github.com/orangain/ktcodeshift"
-  url "https://github.com/orangain/ktcodeshift/releases/download/0.2.2/ktcodeshift-0.2.2.tar.gz"
-  sha256 "99fb24697623553a7ce3f898a5362dd9004ca51d564a657ecf635fba0d0a2aeb"
+  url "https://github.com/orangain/ktcodeshift/releases/download/0.2.3/ktcodeshift-0.2.3.tar.gz"
+  sha256 "9d8f924167d6a3adf744190f2008076cb2b87a4ae805aca298a3fae2c2b7038a"
   license "MIT"
 
   depends_on "openjdk" => [:build, :optional]
